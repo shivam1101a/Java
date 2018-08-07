26c48733e5a4ca81c6a662e606ba1e7ec67f7ab5
shivam1101a <38149931+shivam1101a@users.noreply.github.com>
2018-08-07 14:30:53 +0530

    Create stringcount.java

---
+++ stringcount.java
@@ -0,0 +1,33 @@
+import java.util.*;
+public class stringcount{
+static void prints(String s)
+{
+int n=s.length();
+int freq[]=new int[n];
+int i;
+for(i=0;i<n;i++){
+freq[s.charAt(i)-'a']++;
+}
+for(i=0;i<n;i++){
+if(freq[s.charAt(i)-'a']!=0){
+System.out.print(s.charAt(i));
+				
+System.out.print(freq[s.charAt(i) - 'a'] + " "); 
+				
+freq[s.charAt(i) - 'a'] = 0;
+}
+}
+
+
+
+}
+
+public static void main(String args[])
+{
+String s="aabbccc";
+prints(s);
+
+}
+
+
+}
