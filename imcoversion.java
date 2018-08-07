99f6d9816e48e3ad3fb78caf970863c91b07943b
shivam1101a <38149931+shivam1101a@users.noreply.github.com>
2018-08-07 14:29:42 +0530

    Create imcoversion.java

---
+++ imcoversion.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class imconversion
+{
+    public static void main(String[] args)
+    {
+        int i = 100; 
+        long l = i; 
+        float f = l; 
+        System.out.println("Int value "+i);
+        System.out.println("Long value "+l);
+        System.out.println("Float value "+f);
+    }
+}
