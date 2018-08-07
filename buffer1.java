c4682a6dc4a5ee4689c411f62fc1cfac6afad52c
shivam1101a <38149931+shivam1101a@users.noreply.github.com>
2018-08-07 14:26:38 +0530

    Create buffer1.java

---
+++ buffer1.java
@@ -0,0 +1,16 @@
+import java.io.*;
+class buffer1
+{
+  public static void main(String args[]) throws IOException
+  {
+    char c;
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    System.out.println("Enter characters, press 'q' to quit.");
+
+    do
+    {
+      c = (char) br.read();
+      System.out.println(c);
+    } while(c != 'q');
+  }
+}
